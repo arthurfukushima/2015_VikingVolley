09459e8d3b345474747c60f21a2770afb1be6d06
Arthur Fukushima <arthuryoshinori@gmail.com>
2015-08-11 09:01:07 -0300

    First Prototype
    - Players movement
    - Beach ball
    - Awesome art.

---
+++ SOURCE/Assets/Scripts/_Prot/BallScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallScript : MonoBehaviour 
+{
+	private Rigidbody2D cachedRigidbody;
+
+	void Awake()
+	{
+		cachedRigidbody = GetComponent<Rigidbody2D> ();
+	}
+
+	public void OnCollisionEnter2D(Collision2D pOther)	
+	{
+		if(pOther.transform.CompareTag("Player"))
+		{
+			Vector2 direction =  transform.position - pOther.transform.position ;
+			direction = direction.normalized;
+			//direction.y *= 1.0f;
+			cachedRigidbody.velocity = direction * 8.0f;
+		}
+	}
+}
---
+++ SOURCE/Assets/Scripts/_Prot/BasePlayer.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasePlayer : MonoBehaviour 
+{
+	private Rigidbody2D cachedRigidbody;
+
+	public enum SIDE
+	{
+		LEFT,
+		RIGHT,
+	}
+
+	public SIDE playerSide;
+
+	void OnEnable()
+	{
+		if (cachedRigidbody == null)
+			cachedRigidbody = GetComponent<Rigidbody2D> ();
+
+		EasyTouch.On_TouchDown += OnTouchDown;
+		EasyTouch.On_SimpleTap += OnSimpleTap;
+	}
+
+	void OnDisable()
+	{
+		EasyTouch.On_TouchDown -= OnTouchDown;
+		EasyTouch.On_SimpleTap -= OnSimpleTap;
+	}
+
+	public void Update()
+	{
+
+	}
+
+	void OnTouchDown(Gesture pGesture)
+	{
+		if(playerSide == SIDE.LEFT)
+		{
+			if(pGesture.position.x <= Screen.width / 2.0f)
+			{
+				RaycastHit2D hit2D = Physics2D.Raycast (Camera.main.ScreenToWorldPoint (pGesture.position), Vector2.zero);
+
+				transform.position = Vector3.MoveTowards(transform.position, 
+					new Vector3 (hit2D.point.x, transform.position.y, transform.position.z), 
+					Time.deltaTime * 10.0f);
+			}
+		}
+		else if(playerSide == SIDE.RIGHT)
+		{
+			if(pGesture.position.x > Screen.width / 2.0f)
+			{
+				RaycastHit2D hit2D = Physics2D.Raycast (Camera.main.ScreenToWorldPoint (pGesture.position), Vector2.zero);
+
+				transform.position = Vector3.MoveTowards(transform.position, 
+					new Vector3 (hit2D.point.x, transform.position.y, transform.position.z), 
+					Time.deltaTime * 10.0f);
+			}
+		}
+	}
+
+	void OnSimpleTap(Gesture pGesture)
+	{
+		//cachedRigidbody.AddForce (Vector2.up * 350.0f);
+	}
+}
